05cd622490ea2c13c6bfd10ee36b4d5fb36eef72
Diego Schivo <diego.schivo@gmail.com>
2024-06-22 05:18:46 +0200

    Hpack

--- source/com/janilla/hpack/Hpack.java
+++ source/com/janilla/hpack/Hpack.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.PrimitiveIterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
-import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -15,7 +14,76 @@ import java.util.stream.StreamSupport;
 
 public class Hpack {
 
-	static StaticTable staticTable = new StaticTable();
+	static Table staticTable;
+
+	static {
+		staticTable = new Table();
+		"""
+				:authority=
+				:method=GET
+				:method=POST
+				:path=/
+				:path=/index.html
+				:scheme=http
+				:scheme=https
+				:status=200
+				:status=204
+				:status=206
+				:status=304
+				:status=400
+				:status=404
+				:status=500
+				accept-charset=
+				accept-encoding=gzip=
+				accept-language=
+				accept-ranges=
+				accept=
+				access-control-allow-origin=
+				age=
+				allow=
+				authorization=
+				cache-control=
+				content-disposition=
+				content-encoding=
+				content-language=
+				content-length=
+				content-location=
+				content-range=
+				content-type=
+				cookie=
+				date=
+				etag=
+				expect=
+				expires=
+				from=
+				host=
+				if-match=
+				if-modified-since=
+				if-none-match=
+				if-range=
+				if-unmodified-since=
+				last-modified=
+				link=
+				location=
+				max-forwards=
+				proxy-authenticate=
+				proxy-authorization=
+				range=
+				referer=
+				refresh=
+				retry-after=
+				server=
+				set-cookie=
+				strict-transport-security=
+				transfer-encoding=
+				user-agent=
+				vary=
+				via=
+				www-authenticate=""".lines().map(x -> {
+			var ss = x.split("=", 2);
+			return new Header(ss[0], ss[1]);
+		}).forEach(staticTable::add);
+	}
 
 	public static void main(String[] args) {
 		var e = new Encoder();
@@ -24,41 +92,66 @@ public class Hpack {
 		System.out.println(toBinaryString(e.encodeInteger(1337, 0, 5)));
 		System.out.println(toBinaryString(e.encodeInteger(42, 0, 8)));
 
-		var h = new Header("custom-key", "custom-header");
-		System.out.println(h);
-		var s = toHexString(e.encodeHeader(h));
-		System.out.println(s);
-		var d = new Decoder();
-		d.decodeHeader(parseHex(s).iterator());
-		System.out.println(d.headers);
-		System.out.println(d.dynamicTable);
+		Header h;
+		String s;
+		Decoder d;
 
-		h = new Header(":path", "/sample/path");
-		System.out.println(h);
-		s = toHexString(e.encodeHeader(h));
+//		h = new Header("custom-key", "custom-header");
+//		System.out.println(h);
+//		s = toHexString(e.encodeHeader(h));
+//		System.out.println(s);
+//		d = new Decoder();
+//		d.decodeHeader(parseHex(s).iterator());
+//		System.out.println("dynamicTable=" + d.dynamicTable);
+//		System.out.println("headers=" + d.headers);
+//
+//		h = new Header(":path", "/sample/path");
+//		System.out.println(h);
+//		s = toHexString(e.encodeHeader(h));
+//		System.out.println(s);
+//		d = new Decoder();
+//		d.decodeHeader(parseHex(s).iterator());
+//		System.out.println("dynamicTable=" + d.dynamicTable);
+//		System.out.println("headers=" + d.headers);
+//
+//		h = new Header("password", "secret");
+//		System.out.println(h);
+//		s = toHexString(e.encodeHeader(h));
+//		System.out.println(s);
+//		d = new Decoder();
+//		d.decodeHeader(parseHex(s).iterator());
+//		System.out.println("dynamicTable=" + d.dynamicTable);
+//		System.out.println("headers=" + d.headers);
+//
+//		h = new Header(":method", "GET");
+//		System.out.println(h);
+//		s = toHexString(e.encodeHeader(h));
+//		System.out.println(s);
+//		d = new Decoder();
+//		d.decodeHeader(parseHex(s).iterator());
+//		System.out.println("dynamicTable=" + d.dynamicTable);
+//		System.out.println("headers=" + d.headers);
+
+		List<Header> hh;
+
+		hh = List.of(new Header(":method", "GET"), new Header(":scheme", "http"), new Header(":path", "/"),
+				new Header(":authority", "www.example.com"));
+		System.out.println(hh);
+		s = toHexString(e.encodeHeaderList(hh));
 		System.out.println(s);
 		d = new Decoder();
-		d.decodeHeader(parseHex(s).iterator());
-		System.out.println(d.headers);
-		System.out.println(d.dynamicTable);
+		d.decodeHeaderList(parseHex(s).iterator());
+		System.out.println("dynamicTable=" + d.dynamicTable);
+		System.out.println("headers=" + d.headers);
 
-		h = new Header("password", "secret");
-		System.out.println(h);
-		s = toHexString(e.encodeHeader(h));
+		hh = List.of(new Header(":method", "GET"), new Header(":scheme", "http"), new Header(":path", "/"),
+				new Header(":authority", "www.example.com"), new Header("cache-control", "no-cache"));
+		System.out.println(hh);
+		s = toHexString(e.encodeHeaderList(hh));
 		System.out.println(s);
-		d = new Decoder();
-		d.decodeHeader(parseHex(s).iterator());
-		System.out.println(d.headers);
-		System.out.println(d.dynamicTable);
-
-		h = new Header(":method", "GET");
-		System.out.println(h);
-		s = toHexString(e.encodeHeader(h));
-		System.out.println(s);
-		d = new Decoder();
-		d.decodeHeader(parseHex(s).iterator());
-		System.out.println(d.headers);
-		System.out.println(d.dynamicTable);
+		d.decodeHeaderList(parseHex(s).iterator());
+		System.out.println("dynamicTable=" + d.dynamicTable);
+		System.out.println("headers=" + d.headers);
 	}
 
 	static String toBinaryString(IntStream integers) {
@@ -90,6 +183,11 @@ public class Hpack {
 		List<Header> headers = new ArrayList<>();
 
 		List<Header> dynamicTable = new ArrayList<>();
+
+		void decodeHeaderList(PrimitiveIterator.OfInt bytes) {
+			while (bytes.hasNext())
+				decodeHeader(bytes);
+		}
 
 		void decodeHeader(PrimitiveIterator.OfInt bytes) {
 			var b = bytes.nextInt();
@@ -140,6 +238,23 @@ public class Hpack {
 					headers.add(h);
 					dynamicTable.add(h);
 				}
+			} else if ((b & 0x10) != 0) {
+				String n;
+				{
+					var l = bytes.nextInt();
+					var cc = new char[l];
+					IntStream.range(0, l).forEach(x -> cc[x] = (char) bytes.nextInt());
+					n = new String(cc);
+				}
+				String v;
+				{
+					var l = bytes.nextInt();
+					var cc = new char[l];
+					IntStream.range(0, l).forEach(x -> cc[x] = (char) bytes.nextInt());
+					v = new String(cc);
+				}
+				var h = new Header(n, v);
+				headers.add(h);
 			} else if ((b & 0xf0) == 0) {
 				if ((b & 0x0f) != 0) {
 					var i = decodeInteger(IntStream
@@ -196,25 +311,36 @@ public class Hpack {
 
 	static class Encoder {
 
+		Table dynamicTable = new Table();
+
+		IntStream encodeHeaderList(List<Header> headers) {
+			return headers.stream().flatMapToInt(this::encodeHeader);
+		}
+
 		IntStream encodeHeader(Header header) {
+			var ni = header.name().equals("password");
 			var b = IntStream.builder();
 			var hh = staticTable.headers(header.name());
+			if (hh == null)
+				hh = dynamicTable.headers(header.name());
 			IndexAndHeader h;
 			if (hh != null) {
 				h = hh.stream().filter(x -> x.header().equals(header)).findFirst().orElse(null);
 				if (h != null)
 					encodeInteger(h.index(), 0x80, 7).forEach(b::add);
 				else
-					b.add(hh.get(0).index());
+					encodeInteger(hh.get(0).index(), 0x40, 6).forEach(b::add);
 			} else {
 				h = null;
-				b.add(0x40);
+				b.add(ni ? 0x10 : 0x40);
 				b.add(header.name().length());
 				header.name().chars().forEach(b::add);
 			}
 			if (h == null) {
 				b.add(header.value().length());
 				header.value().chars().forEach(b::add);
+				if (!ni)
+					dynamicTable.add(header);
 			}
 			return b.build();
 		}
@@ -243,96 +369,112 @@ public class Hpack {
 	record IndexAndHeader(int index, Header header) {
 	};
 
-	static class StaticTable {
+//	static class StaticTable {
+//
+//		List<Header> list;
+//
+//		Map<String, List<IndexAndHeader>> map;
+//
+//		public StaticTable() {
+//			list = """
+//					:authority=
+//					:method=GET
+//					:method=POST
+//					:path=/
+//					:path=/index.html
+//					:scheme=http
+//					:scheme=https
+//					:status=200
+//					:status=204
+//					:status=206
+//					:status=304
+//					:status=400
+//					:status=404
+//					:status=500
+//					accept-charset=
+//					accept-encoding=gzip=
+//					accept-language=
+//					accept-ranges=
+//					accept=
+//					access-control-allow-origin=
+//					age=
+//					allow=
+//					authorization=
+//					cache-control=
+//					content-disposition=
+//					content-encoding=
+//					content-language=
+//					content-length=
+//					content-location=
+//					content-range=
+//					content-type=
+//					cookie=
+//					date=
+//					etag=
+//					expect=
+//					expires=
+//					from=
+//					host=
+//					if-match=
+//					if-modified-since=
+//					if-none-match=
+//					if-range=
+//					if-unmodified-since=
+//					last-modified=
+//					link=
+//					location=
+//					max-forwards=
+//					proxy-authenticate=
+//					proxy-authorization=
+//					range=
+//					referer=
+//					refresh=
+//					retry-after=
+//					server=
+//					set-cookie=
+//					strict-transport-security=
+//					transfer-encoding=
+//					user-agent=
+//					vary=
+//					via=
+//					www-authenticate=""".lines().map(x -> {
+//				var ss = x.split("=", 2);
+//				return new Header(ss[0], ss[1]);
+//			}).toList();
+//
+//			map = list.stream().collect(
+//					Collector.of(HashMap::new, new BiConsumer<HashMap<String, List<IndexAndHeader>>, Header>() {
+//
+//						int index;
+//
+//						@Override
+//						public void accept(HashMap<String, List<IndexAndHeader>> t, Header u) {
+//							t.compute(u.name(), (k, v) -> {
+//								if (v == null)
+//									v = new ArrayList<>();
+//								v.add(new IndexAndHeader(++index, u));
+//								return v;
+//							});
+//						}
+//					}, (m1, m2) -> m1));
+//
+//		}
+//
+//		Header header(int index) {
+//			var i = index - 1;
+//			return i >= 0 && i < list.size() ? list.get(index - 1) : null;
+//		}
+//
+//		List<IndexAndHeader> headers(String name) {
+//			return map.get(name);
+//		}
+//	}
 
-		List<Header> list;
+	static class Table {
 
-		Map<String, List<IndexAndHeader>> map;
+		List<Header> list = new ArrayList<>();
 
-		public StaticTable() {
-			list = """
-					:authority=
-					:method=GET
-					:method=POST
-					:path=/
-					:path=/index.html
-					:scheme=http
-					:scheme=https
-					:status=200
-					:status=204
-					:status=206
-					:status=304
-					:status=400
-					:status=404
-					:status=500
-					accept-charset=
-					accept-encoding=gzip=
-					accept-language=
-					accept-ranges=
-					accept=
-					access-control-allow-origin=
-					age=
-					allow=
-					authorization=
-					cache-control=
-					content-disposition=
-					content-encoding=
-					content-language=
-					content-length=
-					content-location=
-					content-range=
-					content-type=
-					cookie=
-					date=
-					etag=
-					expect=
-					expires=
-					from=
-					host=
-					if-match=
-					if-modified-since=
-					if-none-match=
-					if-range=
-					if-unmodified-since=
-					last-modified=
-					link=
-					location=
-					max-forwards=
-					proxy-authenticate=
-					proxy-authorization=
-					range=
-					referer=
-					refresh=
-					retry-after=
-					server=
-					set-cookie=
-					strict-transport-security=
-					transfer-encoding=
-					user-agent=
-					vary=
-					via=
-					www-authenticate=""".lines().map(x -> {
-				var ss = x.split("=", 2);
-				return new Header(ss[0], ss[1]);
-			}).toList();
-
-			map = list.stream().collect(
-					Collector.of(HashMap::new, new BiConsumer<HashMap<String, List<IndexAndHeader>>, Header>() {
-
-						int index;
-
-						@Override
-						public void accept(HashMap<String, List<IndexAndHeader>> t, Header u) {
-							t.compute(u.name(), (k, v) -> {
-								if (v == null)
-									v = new ArrayList<>();
-								v.add(new IndexAndHeader(++index, u));
-								return v;
-							});
-						}
-					}, (m1, m2) -> m1));
-
-		}
+		Map<String, List<IndexAndHeader>> map = new HashMap<>();
 
 		Header header(int index) {
 			var i = index - 1;
@@ -341,6 +483,16 @@ public class Hpack {
 
 		List<IndexAndHeader> headers(String name) {
 			return map.get(name);
+		}
+
+		void add(Header header) {
+			list.add(header);
+			map.compute(header.name(), (k, v) -> {
+				if (v == null)
+					v = new ArrayList<>();
+				v.add(new IndexAndHeader(list.size(), header));
+				return v;
+			});
 		}
 	}
 }
